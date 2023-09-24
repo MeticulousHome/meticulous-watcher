f2d85d4d30a8b4f7bcaa4974ae1bb670b8a026b1
Angel Padilla <angel.p.lopez@outlook.com>
2023-09-24 15:50:01 -0600

    feature(watcher):lil back and pipes implementation The "lil back" serves as a miniature backup of the backend that allows to keep a brief communication with the front end going to notify about a problem when the backend seems to be not responding
    The pipes are streams that allow the watcher communicate with
    the backend and viceversa so it can see when the backend goes offline
    due to any trouble. It can also use them to ask the backend to
    free the reources needed to update the ESP firmware

--- watcher.py
+++ watcher.py
@@ -49,18 +49,26 @@
 
 
 from tornado.options import define, options, parse_command_line
-import socketio
 import tornado.web
 import tornado.ioloop
 import hashlib
-import base64
 import os
 import subprocess
 import time
 import threading
 import traceback
+import stat
+import socketio
+import asyncio
 
-# CLASS DEFINITION
+user_path=os.path.expanduser("~/")
+
+##################################################################################################################
+##################################################################################################################
+##################################################################################################################
+##################################################################################################################
+##################################################################################################################
+# HTTP SERVER HANDLING
 class UploadHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "http://192.168.50.10:3000")     #allows requests from the dashboard
@@ -97,48 +105,61 @@ class UploadHandler(tornado.web.RequestHandler):
         self.finish()
         stopESPcomm = True
 
-# GLOBAL VARIABLES
-autoupdate_path = "./meticulous-raspberry-setup/meticulous-autoupdate"
-user_path=os.path.expanduser("~/")
 
-updating = False
 
+##################################################################################################################
+# IPC HANDLERS
+IPC_path = f'{user_path}/ipc'                              # directory for the InterProcess Communication pipes
 pipe1 = None
 pipe2 = None
-
-pipe1_path = f''
-pipe2_path = f''
-
-message = bytes()
+pipe2_path = f'{IPC_path}/pipe2'
+pipe1_path = f'{IPC_path}/pipe1'
 backAlive = False
-continueUpdate = False
-
+message = bytes()
 backend_time_off = 5   #seconds the backend is allowed to be offline in one occurrence
-
-
-# THREAD VARIABLES
 read_back_thread = None
 
-# FUNCTION DEFINITIONS
+#this function creates the pipes to allow communication between backend and the watcher
+def checkPipes():
+    #validates if the pipes directory exists
+    if os.path.exists(IPC_path):
+
+        #validates the file repersenting pipe1 exists
+        if os.path.exists(pipe1_path):
+
+            #checks if it is indeed a pipe
+            pipe1_stat = os.stat(pipe1_path)
+
+            #if its not a pipe, it deletes the file and creates a pipe
+            if not stat.S_ISFIFO(pipe1_stat.st_mode):
+                os.remove(pipe1_path)
+                os.mkfifo(pipe1_path)
+        #if the file representing a pipe does not exist
+        else:
+            #it's created
+            os.mkfifo()
+
+        #repeats with pipe 2
+        if os.path.exists(pipe2_path):
+
+            pipe2_stat = os.stat(pipe2_path)
+            
+            if not stat.S_ISFIFO(pipe2_stat.st_mode):
+                os.remove(pipe2_path)
+                os.mkfifo(pipe2_path)
+        else:
+            os.mkfifo()
+
+    #if not even the directory exists
+    else:
+        #create the base directory
+        os.mkdir(IPC_path)
+        #create the pipes
+        os.mkfifo(pipe1_path)
+        os.mkfifo(pipe2_path)
 
 
-def createUpdateDir():
-    # Specify the directory path you want to create
-    directory_path = os.path.expanduser("~/update")
-
-    # Check if the directory already exists
-    if not os.path.exists(directory_path):
-    # Create the directory if it does not exist
-        os.makedirs(directory_path)
-        #print(f"Directory '{directory_path}' created successfully.")
-
-#this function opens a pipe to allow communication between backend and the watcher
-def openPipes():
-    try:
-        pipe2 = os.open(pipe2_path, os.O_RDONLY | os.O_NONBLOCK)
-        pipe1 = os.open(pipe1_path, os.O_WRONLY)
-    except OSError as e:
-        print(f'an error occurred oppening pipes: {e}')
+##### BACKEND CHECKER
 
 # This function kepps track that the backend is still alive or not and
 # updates the flag accordignly. It contains no logic to handle any case
@@ -146,10 +167,16 @@ def readBackend():
     global message
     global backAlive
     global continueUpdate
-    global first_backend_fail_time
+    global pipe2
+
+    try:
+        pipe2 = os.open(pipe2_path, os.O_RDONLY | os.O_NONBLOCK)
+    except OSError as e:
+        print(f'an error occurred oppening pipe2: {e}')
+        pipe2 = None
 
     while True:
-        if pipe1 != None:
+        if pipe2 != None:
             message = os.read(pipe1, 1024)
             if message:
                 backAlive = True
@@ -157,7 +184,6 @@ def readBackend():
                     continueUpdate = message.decode() == "released"
             else:
                 backAlive = False
-
 
 #This functions checks if the backend has been offline more than it should
 def backendFail():
@@ -168,9 +194,11 @@ def backendFail():
         time.sleep(backend_time_off)
         return (not backAlive) and (not updating)
 
-# This function checks if the failure was not isolated
+# This function checks if the failure was not isolated (this must be a task in a thread)
 def backendDead():
     failure_count = 0
+    restarts_done = 0
+
     while True:
         if backendFail():
             failure_count = failure_count + 1
@@ -178,20 +206,74 @@ def backendDead():
             failure_count = 0
 
         if failure_count > 4:
-            #Se make sure the backend procecss is dead
-            subprocess.run("systemctl stop back.service ",shell=True,capture_output=True,text=True,cwd=user_path)
+            failure_count = 0
+            # We restart the backend up to 3 times
+            if restarts_done < 3:
+                restarts_done = restarts_done + 1
+                subprocess.run("systemctl stop back",shell=True,capture_output=True,text=True,cwd=user_path)
+                time.sleep(1)
+                subprocess.run("systemctl start back",shell=True,capture_output=True,text=True,cwd=user_path)
+            #if we have tried to restart the back 3 times and still dead
+            else:
+
+                #stop the back as we will need the por 8080 to be free, just making sure of it
+                subprocess.run("systemctl stop back",shell=True,capture_output=True,text=True,cwd=user_path)
+
+                #launch the little back to notify the need to restart / upgrade the machine
+                lil_back("fail")
+
             #Notify the user to restart the Meticulous or upload software
             #START LIL BACKEND
             #NOTIFY USER TO RESTART OR UPLOAD
 
+# This function boots the lil back to advise the user a problem has occured and ask for a restart or update
+def lil_back(notify:str):
+    # create a new HTTP aplication in the server the back should have been
+    lil_sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='tornado')
+
+    #serves the app for socket communication 
+    lil_app = tornado.web.Application(
+        [
+            (r"/socket.io/", socketio.get_tornado_handler(lil_sio)),
+        ],
+    )
+
+    time.sleep(2)
+    
+    #on the port that the backend was using
+    lil_app.listen(8080)
+
+    #notifies the frontend that the backend failed
+    if notify == "fail":
+        asyncio.run(lil_sio.emit("BACKEND_FAIL"))
+    if notify == "update":
+        asyncio.run(lil_sio.emit("MANUAL-REBOOT"))
+
+##################################################################################################################
+# UPDATE HANDLERS
+autoupdate_path = "./update/Script" 
+updating = False
+continueUpdate = False
+
+#This function creates the update directory where the file will be stored and extracted
+def createUpdateDir():
+    # Specify the directory path you want to create
+    directory_path = os.path.expanduser("~/update")
+
+    # Check if the directory already exists
+    if not os.path.exists(directory_path):
+    # Create the directory if it does not exist
+        os.makedirs(directory_path)
+        #print(f"Directory '{directory_path}' created successfully.")
+
+#This function starts the update process
 def startUpdate():
 
-    global stopESPcomm
     global reboot_flag
     global continueUpdate
     global updating
 
-    stopESPcomm = True
+    updating = True  # affects: 
 
     path = "./update/updtPckg.tar.gz"
 
@@ -204,23 +286,25 @@ def startUpdate():
     subprocess.run(command, shell=True,cwd=user_path)
 
     # ASK BACKEND TO FREE RESOURCES
-
+    with open(pipe1_path, 'w') as pipe:
+        pipe.write("FREE")
     # WAIT FOR THE BACKEND CONFIRMATION THAT RESOURCES ARE FREED (and its dead)
+    while not continueUpdate:
+        pass
 
-    #call the update script (will use the script as a module)
-    command = f'python {autoupdate_path}/update_protocol.py'
+    #call the update script that will be provided in the update pckg
+    command = f'python3 {autoupdate_path}/update_protocol.py'
     update_success = subprocess.run(command, shell=True, capture_output=True, text=True,cwd=user_path).stdout
 
     print(update_success)
 
-    reboot_flag = True
-    time.sleep(2)
-    PID = subprocess.run("systemctl status back.service | grep -oP 'Main PID: \K\d+'",shell=True,capture_output=True,text=True,cwd=user_path).stdout
+    #We restart the backend
+    subprocess.run("systemctl stop back",shell=True,capture_output=True,text=True,cwd=user_path)
+    time.sleep(1)
+    subprocess.run("systemctl start back",shell=True,capture_output=True,text=True,cwd=user_path)
 
-    #y lo matamos alv _(~o _ o~)_/\_(0 _ 0)_
-
-    subprocess.run(f'sudo kill -9 {PID}',shell=True,cwd=user_path)
     updating = False
+
 
 def main():
     global read_back_thread
@@ -228,7 +312,7 @@ def main():
     parse_command_line()
 
     #opens the pipes to chat with backend
-    openPipes()
+    checkPipes()
 
     #starts the thread that reads the pipe2
     read_back_thread = threading.Thread(target=readBackend)
